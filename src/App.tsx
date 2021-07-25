7673309f386a6cc19d20b60618f3cf5ac491a4f1
Ratul <ratulkhan1428@gmail.com>
2021-07-25 15:52:12 +0600

    first react typescript project done

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Styles
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+import { Wrapper } from './styles/App.styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+type BitcoinData = {
+  '15m': number;
+  buy: number;
+  last: number;
+  sell: number;
+  symbol: string;
+}
+
+type Currencies = {
+  [key: string]: BitcoinData;
+}
+
+const getBCData = async (): Promise<Currencies> =>
+  await (await fetch('https://blockchain.info/ticker')).json();
+
+const INTERVAL_TIME = 1000;
+
+const App = () => {
+  const [currency, setCurrency] = useState('USD');
+  const { data, isLoading, error } = useQuery<Currencies>(
+    'bc-data', 
+    getBCData,
+    {
+      // Refetch the data every second
+      refetchInterval: INTERVAL_TIME
+    }
+  );
+  
+  const handleCurrencySelection = (e: any) => {
+    setCurrency(e.currentTarget.value);
+  }
+
+  // useEffect(() => {
+  //   const interval = setInterval(refetch, INTERVAL_TIME);
+  //   return () => clearInterval(interval);
+  // }, [refetch])
+
+  if (isLoading) return <h3>Loading...</h3>
+  if (error) return <h3>Something went wrong!</h3>
+
+  return(
+    <Wrapper>
+      <>
+        <h2>Bitcoin Price</h2>
+        <select value={currency} onChange={handleCurrencySelection}>
+          {data && Object.keys(data).map(currency => (
+            <option key={currency} value={currency}>  
+              {currency}
+            </option>
+          ))}
+        </select>
+        <div>
+          <h2>
+            {data && data[currency].symbol}: {data && data[currency].last}
+          </h2>
+        </div>
+      </>
+    </Wrapper>
   );
 }
 
